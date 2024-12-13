494fcd68904947e44f23ece8aebe02ad1bfc72db
loic lefloch <loic.lefloch.dev@gmail.com>
2024-12-13 17:32:42 +0100

    fix image caption

--- themes/cactus/scripts/caption.js
+++ themes/cactus/scripts/caption.js
@@ -1,9 +1,12 @@
-// https://github.com/wayou/hexo-image-caption/blob/master/index.js
-hexo.extend.filter.register('after_post_render', function(data) {
-  if (hexo.config.image_caption && hexo.config.image_caption.enable == true) {
-    var className = hexo.config.image_caption.class_name || 'image-caption';
-    if (data.layout == 'post' || data.layout == 'page' || data.layout == 'about') {
-      data.content = data.content.replace(/(<div><img [^>]*alt="([^"]+)"[^>]*>)/g, '$1' + '<div class="' + className + '">$2</div></div>');
+// https://github.com/thom4parisot/hexo-image-caption/blob/patch-1/index.js
+hexo.extend.filter.register('after_post_render', function (data) {
+  if (hexo.config.image_caption.enable === true) {
+    const {class_name = 'image-caption'} = hexo.config.image_caption;
+    if (data.layout === 'post' || data.layout === 'page' || data.layout === 'about') {
+      data.content = data.content.replace(
+        /(<img [^>]*alt="([^"]+)"[^>]*>)/g,
+        `<figure class="${class_name}">$1<figcaption>$2</figcaption></figure>`
+      );
     }
   }
   return data;
